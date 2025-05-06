0c1b8f72e087796fe4002e0aebfd86d2fe22c115
abdulla615 <moulanasyd108@gmail.com>
2025-05-07 00:50:39 +0530

    updated

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,5 @@
-import json
-
 def lambda_handler(event, context):
-    # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! From Git Hub Action.. uodated the commit updated the code')
+        'body': 'Hello from Lambda!'
     }
